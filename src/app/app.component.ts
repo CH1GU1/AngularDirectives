2d5281e7c50325d3042ca1b86d64b47f12738f8a
Kevin <kamcak@hotmail.es>
2022-06-26 22:39:57 -0500

    Directives added, ngStyle and ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directiveWork';
+  title = 'Registro de Usuarios';
+  message = "";
+  registered = false;
+  name:string = "";
+  lastName:string = "";
+  position:string="";
+  entries: any;
+
+  constructor(){
+
+    this.entries = [
+
+      {title:"Java es muy melo"},
+      {title:"Ing Telemática papá"},
+      {title:"Visual Studio"},
+      {title:"Intellij"},
+      {title:"Aviation"},
+
+    ]
+
+
+
+  }
+
+  registerUser(){
+
+    this.registered = true;
+
+    this.message = "Registered user succesfully";
+
+
+  }
 }
